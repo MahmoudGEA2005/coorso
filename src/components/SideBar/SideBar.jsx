4947551c547e9d45f3e04c5e7693e2d6727262cb
MahmoudGEA2005 <mahmoud.ea2005@gmail.com>
2025-06-05 01:19:16 +0300

    fix bug

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -16,7 +16,7 @@ const SideBar = ({
 }) => {
   const setPrice = (data) => {
     setPriceFilter([data.min || 0, data.max || 9000000]);
-    isOpened(false);
+    open(false);
   };
   const { register, handleSubmit } = useForm();
   const includeHandler = (ins) => {
